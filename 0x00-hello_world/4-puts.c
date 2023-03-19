27a0fd74bee6b5c8bb38be91b23e6b20274c0dda
laola24462 <olanrewaju.toyeeb77@gmail.com>
2023-03-18 20:50:41 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - main entry point
- *return : always 0 (success)
- */
+  * main - Entry
+  * Return: always 0
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
